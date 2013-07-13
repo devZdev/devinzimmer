986ff2c95911531eabfe81417a575a77a5742465
Devin Zimmer <dz@renttherunway.com>
2013-07-12 22:05:34 -0400

    mongodb connections for dev and prod

--- app.rb
+++ app.rb
@@ -7,20 +7,25 @@ module DZ
 
 		def get_connection
 				return @db_connection if @db_connection	
-			db = URI.parse(ENV['MONGOHQ_URL'])
-			db_name = db.path.gsub(/^\//, '')
-		
-			@db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
-			@db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
-  			@db_connection
+
+			if ENV['RACK_ENV'] != 'development' then
+				db = URI.parse(ENV['MONGOHQ_URL'])
+				db_name = db.path.gsub(/^\//, '')
+				@db_connection = Mongo::MongoClient.new(db.host, db.port).db(db_name)
+				@db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+	  			@db_connection
+  			else
+  				@db_connection = Mongo::MongoClient.new("localhost", 27017)
+  				@db_connection
+  			end
 		end
 
 		get '/' do
 			db = get_connection
-			puts "Collections"
+			puts "Databases"
 			puts "==========="
-			collections = db.collection_names
-			puts collections
+			databases = db.database_names
+			puts databases
 	  		erb :layout
 		end
 	end
