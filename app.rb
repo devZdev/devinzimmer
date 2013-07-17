22b80b8dfb6eb582a25ce47c047dc76230339f4c
Devin Zimmer <dz@renttherunway.com>
2013-07-17 10:47:28 -0400

    inserting and removing test posts

--- app.rb
+++ app.rb
@@ -27,7 +27,14 @@ module DZ
 	  		erb :layout, :locals => {:posts => posts}
 		end
 
-		get '/add' do
+		get '/posts' do
+			db = get_db_connection
+			posts_collection = db['posts']
+			posts = posts_collection.find.each.to_a
+	  		erb :layout, :locals => {:posts => posts}
+		end
+
+		get '/posts/add_test_post' do
 			post = {"author" => "Sinatra", "title" => "Test Post Title", "body" => "Test Post Body" }
 			db = get_db_connection
 			posts_collection = db['posts']
@@ -35,5 +42,14 @@ module DZ
 			posts = posts_collection.find.each.to_a
 			erb :layout, :locals => {:posts => posts}
 		end
+
+		get '/posts/delete_test_posts' do
+			param = {"author" => "Sinatra"}
+			db = get_db_connection
+			posts_collection = db['posts']
+			posts_collection.remove(param)
+			posts = posts_collection.find.each.to_a
+			erb :layout, :locals => {:posts => posts}
+		end 
 	end
 end
