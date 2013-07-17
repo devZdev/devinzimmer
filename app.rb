0f54854086659e0d92212d28d1e0aa32238d6885
Devin Zimmer <dz@renttherunway.com>
2013-07-17 10:25:02 -0400

    mongodb reading/writing; testing heroku too

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'uri'
 module DZ
 	class App < Sinatra::Application
 
-		def get_connection
+		def get_db_connection
 				return @db_connection if @db_connection	
 
 			if ENV['RACK_ENV'] != 'development' then
@@ -15,17 +15,25 @@ module DZ
 				@db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
 	  			@db_connection
   			else
-  				@db_connection = Mongo::MongoClient.new("localhost", 27017)
+  				@db_connection = Mongo::MongoClient.new("localhost", 27017).db('db-mongodb-dev')
   				@db_connection
   			end
 		end
 
 		get '/' do
-			db = get_connection
-			puts "Database OBJ"
-			puts "==========="
-			puts db
-	  		erb :layout
+			db = get_db_connection
+			posts_collection = db['posts']
+			posts = posts_collection.find.each.to_a
+	  		erb :layout, :locals => {:posts => posts}
+		end
+
+		get '/add' do
+			post = {"author" => "Sinatra", "title" => "Test Post Title", "body" => "Test Post Body" }
+			db = get_db_connection
+			posts_collection = db['posts']
+			posts_collection.insert(post)
+			posts = posts_collection.find.each.to_a
+			erb :layout, :locals => {:posts => posts}
 		end
 	end
 end
